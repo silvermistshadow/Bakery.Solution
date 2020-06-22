fcbafe4f4bc3283fd38660a5e1dd5ec890ed62b9
silvermistshadow <bmead72@gmail.com>
2020-06-22 10:26:47 -0700

    add code to models

---
+++ Bakery/Models/Order.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+    public class Order
+    {
+        private static List<Order> _instances = new List<Order>();
+        public int Id { get; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Order(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            _instances.Add(this);
+            Id = _instances.Count;
+        }
+
+        public static List<Order> GetAll()
+        {
+            return _instances;
+        }
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+
+        public static Order Find(int searchId)
+        {
+            return _instances[searchId-1];
+        }
+    }
+}
---
+++ Bakery/Models/Vendor.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models 
+{
+    public class Vendor
+    {
+        private static List<Vendor> _instances = new List<Vendor>();
+        public string Name { get; set; }
+        public int Id { get; }
+        public string Description { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Vendor(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Orders = new List<Order>();
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+        public static List<Vendor> GetAll()
+        {
+            return _instances;
+        }
+        public static Vendor Find(int searchId)
+        {
+            return _instances[searchId-1];
+        }
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+    }
+}
